bc07b143fe71c9758550b83436d6bddbbc14f39f
Bento Borges Schirmer <bbschirmer@inf.ufsm.br>
2023-12-13 17:18:16 -0300

    desavanços

--- nicolas-cage.py
+++ nicolas-cage.py
@@ -4,49 +4,36 @@ from mitmproxy import http
 from mitmproxy.addonmanager import Loader
 from mitmproxy.log import ALERT
 from aux_info.src_images import images
+from badwords import manager
+import random
 
 logger = logging.getLogger(__name__)
 
-#badwords = open("badwords.txt", "r").get_value().split("\n")
+le_manager = manager()
 #["addon", "method", "TCP", "request"]
 image_types = [".jpg", ".png", ".webp", ".svg", ".ico", ".gif"]
 
-<<<<<<< HEAD
-#def load(loader: mitmproxy.addonmanager.Loader):
-   
-def done():
-    dumper.close()
-=======
-def load(loader: Loader):
-    logger.info("This is some informative text.")
-    logger.warning("This is a warning.")
-    #logger.error("This is an error.")
-    logger.log(
-        ALERT,
-        "This is an alert. It has the same urgency as info, but will also pop up in the status bar.",
-    )
->>>>>>> 014f4a02b7922ebccf334f5001a819905afff96d
-
 """Modify an HTTP form submission."""
 def request(flow: http.HTTPFlow) -> None:
-<<<<<<< HEAD
     if any(i in flow.request.path for i in image_types):
-        flow.request.url = "https://img.olhardigital.com.br/wp-content/uploads/2023/05/super-homem-nicolas-cage.png"
+        flow.request.url = images.get_random_image()
 
 def response(flow: http.HTTPFlow) -> None:
     body = flow.response.get_text(False).lower()
-#    for badword in badwords:
-#        if badword in body:
-            #flow.response.set_text(body.replace(badword, "noggers"))
-=======
-    logger.info(f"[poggers] {flow.request.url}")
+    #if "Content-Type" in  flow.response.headers and "html" in flow.response.headers["Content-Type"]:
 
-    logger.info(flow.request.path)
-    logger.info(type(flow.request.path))
-    if ".jpg" in flow.request.url or ".png" in flow.request.url or ".webp" in flow.request.url or ".svg" in flow.request.url:
-        flow.request.url = images.get_random_image()
-        logger.info(f"[righers] {flow.request.url}")
->>>>>>> 014f4a02b7922ebccf334f5001a819905afff96d
+    logger.info(flow.response.headers["Content-Type"])
+    #with open("penis.txt", "a") as file:
+    #    file.write(flow.request.pretty_host)
+    #    file.write(flow.response.get_text(False))
+    for badword in le_manager.badwords:
+        goodword = random.choice(le_manager.goodwords)
+        body = body.replace(badword.lower(), goodword)
+    flow.response.set_text(body)
+
+    #logger.info(f"there are {len(le_manager.badwords)} badwords and {len(le_manager.goodwords)}")
+        #    if badword.lower() in body:
+        #        flow.response.set_text(body.replace(badword.lower(), random.choice(le_manager.goodwords)))
 
 
 
