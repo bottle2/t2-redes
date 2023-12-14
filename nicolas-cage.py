72f9ce59850c2a91abc41ea6c5d8698e3731fc0a
bottle2 <bento.schirmer@gmail.com>
2023-12-14 20:49:29 -0300

    NICOLASCAGE

--- nicolas-cage.py
+++ nicolas-cage.py
@@ -3,10 +3,14 @@ from mitmproxy import http
 
 from mitmproxy.addonmanager import Loader
 from mitmproxy.log import ALERT
-from aux_info.src_images import images
+from mitmproxy import command
+from aux_info.src_images import ImageList
+from aux_info.src_images import Image
 import random
 
 logger = logging.getLogger(__name__)
+
+images = ImageList()
 
 def is_image(accept):
     registries = [
@@ -17,3 +21,19 @@ def is_image(accept):
 def request(flow: http.HTTPFlow) -> None:
     if "Accept" in flow.request.headers and is_image(flow.request.headers["Accept"]):
         flow.request.url = images.get_random_image_path()
+
+class nicolas:
+    @command.command("nicolas.list")
+    def print(self):
+        logger.info(f"Got {len(images.images)} images")
+        images.print_images(logger)
+
+    @command.command("nicolas.set")
+    def set(self, name : str, url : str):
+        images.add_image(Image(name, url))
+
+    @command.command("nicolas.del")
+    def dele(self, name : str):
+        images.remove_image(name)
+
+addons = [nicolas()]
